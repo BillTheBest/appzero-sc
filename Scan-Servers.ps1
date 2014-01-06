00da4a102adbfe5c04b6e1b57fc04113f98e72fa
Giovanni Boschi <giovanniboschi@gmail.com>
2014-01-05 21:47:32 -0500

    added scan script

---
+++ Scan-Servers.ps1
@@ -0,0 +1,25 @@
+﻿# Copyright (c) 2013 AppZero Software Corporation.  All Rights Reserved.
+#
+# Executes a scan of multiple servers to build out a pace database for
+# subsequent selection and migration
+
+Param(
+    [Parameter(Mandatory=$true)]
+    [string] $filename
+)
+
+$appzpace = $Env:AppZero_Path + "appzpace.exe"
+
+# Get installed program lists using appzpace
+Function Get-InstalledPrograms([string]$credentialsFile)
+{
+    & $appzpace /M /L $credentialsFile |
+        tee -Variable output | Out-Host
+}
+
+Get-InstalledPrograms -credentialsFile $filename
+
+
+
+
+
