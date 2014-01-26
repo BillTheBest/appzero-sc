d48f5035e1f1c6e116a2fa2d3b0d7b4a31b6a0ca
Giovanni Boschi <giovanniboschi@gmail.com>
2014-01-26 09:29:54 -0500

    -added Remove-PaceData and Sync-PaceRepo -added SCO "activity wrappers" for Scan-Servers and Classify-Apps

---
+++ psh/Get-SourceServers.ps1
@@ -0,0 +1,21 @@
+﻿# Copyright (c) 2013 AppZero Software Corporation.  All Rights Reserved.
+#
+# Executes a scan of multiple servers to build out a pace database for
+# subsequent selection and migration
+
+Param(
+    [Parameter(Mandatory=$true)]
+    [string] $root,
+    [Parameter(Mandatory=$true)]
+    [string] $stagingHost
+)
+
+
+return Get-ChildItem -Path "$root\servers\$stagingHost\PACE" -Name
+
+
+
+
+
+
+
--- psh/Scan-Servers.ps1
+++ psh/Scan-Servers.ps1
@@ -7,21 +7,31 @@ Param(
     [Parameter(Mandatory=$true)]
     [string] $root,
     [Parameter(Mandatory=$true)]
+    [string] $stagingHost,
+    [Parameter(Mandatory=$true)]
     [string] $filename
+    
 )
 
 $appzpace = $Env:AppZero_Path + "appzpace.exe"
 
+$fullpath = Resolve-Path $filename
+$log = Join-Path -Path (Split-Path -Parent $fullpath) -ChildPath "PaceLog.txt"
+
 # Get installed program lists using appzpace
 Function Get-InstalledPrograms([string]$credentialsFile)
 {
-    & $appzpace /M /L $credentialsFile
+    & $appzpace /M /L $credentialsFile |
+        Out-File $log -Append
+    $sources = Get-ChildItem -Path $root\servers\$stagingHost\PACE -Name
+    return $sources
 }
 
 # appzpace /L /M wants to run in the install dir
 #  resolve the filepath from cwd first
-$fullpath = Resolve-Path $filename
+
 Get-InstalledPrograms -credentialsFile $fullpath
+
 
 
 
