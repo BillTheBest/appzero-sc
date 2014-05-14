a5f958bdf2cdd371cf38c41f9583f02b9a91b245
Giovanni Boschi <giovanniboschi@gmail.com>
2014-05-14 01:57:22 -0400

    Activity scripts for each custom activity type

---
+++ Modules/AppZeroActivity.psm1
@@ -0,0 +1,26 @@
+﻿# Copyright (c) 2013 AppZero Software Corporation.  All Rights Reserved.
+#
+
+Function New-AZRemoteSession
+(
+    [Parameter(Mandatory=$true)]
+    [string]$TargetHost,
+    [Parameter(Mandatory=$true)]
+    [string]$LocalBasePath,
+    [Parameter(Mandatory=$true)]
+    [string]$TargetPath
+)
+{
+    $dotPacePath = Join-Path -Path $LocalBasePath -ChildPath ".pace"
+    $credsPath = Join-Path -Path $dotPacePath -ChildPath "$TargetHost.creds"
+    $creds = Import-Clixml $credsPath
+    $sess = New-PSSession -cn $TargetHost -Credential $creds
+    
+    $result = Invoke-Command -Session $sess -ScriptBlock {
+        Param($stgpath,$stghost)
+        Import-Module $stgpath\psh\AppZero.psm1 -ArgumentList $stgpath,$stghost
+        Import-Module $stgpath\psh\AppZeroTag.psm1
+    } -ArgumentList $TargetPath,$TargetHost
+    
+    return $sess
+}
---
+++ sco/Activity-Install-AppZero.ps1
@@ -0,0 +1,20 @@
+﻿# Copyright (c) 2013 AppZero Software Corporation.  All Rights Reserved.
+#
+# Set up a new PACE working repo
+
+Param(
+    [string]$targetHost = "prod1",
+    [string]$version = "5.4SP1.1",
+    [string]$targetPath = "c:\appzero-sco",
+    [string]$targetHostUser = "Administrator",
+    [string]$targetHostPassword = "P@ssw0rd",
+    [string]$stagingShare = "\\sco\appzero-field",
+    [string]$stagingShareUser = "sco\Administrator",
+    [string]$stagingSharePassword = "P@ssw0rd"
+)
+
+
+cd C:\Users\Administrator\dev\appzero-field
+Import-Module .\psh\AppZeroWorkflow.psm1
+Install-AppZero $targetHost $version $targetPath $targetHostUser $targetHostPassword $stagingShare $stagingShareUser $stagingSharePassword
+
